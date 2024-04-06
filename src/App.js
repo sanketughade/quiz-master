328e25d32a9b9eb94e9dd461b02379fbdc439962
Sanket Ughade <sanketughade@Sankets-MacBook-Air.local>
2024-04-06 13:58:01 +0530

    Completed the static design part.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,62 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Question />
+      <Options />
+    </div>
+  );
+}
 
+function Header() {
+  return (
+    <div className="header">
+      <div className="vertical-flex-container question-progress">
+        <p>Question 1/5</p>
+        <div className="question-progress-bar">
+          <div className="progress-loaded"></div>
+        </div>
+      </div>
+      <div className="vertical-flex-container score">
+        <p>Score</p>
+        <p className="bold-font">0</p>
+      </div>
+    </div>
+  );
+}
+
+function Question() {
+  return (
+    <p className="question-text">In web design what does CSS stand for?</p>
+  );
+}
+
+function Options() {
+  return (
+    <ul className="options">
+      <li className="option">
+        <Option option={{ id: 'A', label: 'Counter Strike: Source' }} />
+      </li>
+      <li className="option">
+        <Option option={{ id: 'B', label: 'Corrective Style Sheet' }} />
+      </li>
+      <li className="option">
+        <Option option={{ id: 'C', label: 'Computer Style Sheet' }} />
+      </li>
+      <li className="option">
+        <Option option={{ id: 'D', label: 'Cascading Style Sheet' }} />
+      </li>
+    </ul>
+  );
+}
+
+function Option({ option }) {
+  return (
+    <button className="option-btn">
+      <div className="option-id">
+        <span className="option-id-text">{option.id}</span>
+      </div>
+      <span className="option-label">{option.label}</span>
+    </button>
+  );
+}
